5a7e22fd0647e11ea98d3e31e0caac76a8cf62d9
Jinzhu <wosmvp@gmail.com>
2017-10-23 14:13:20 +0800

    Update register processor, validator API

--- exchange_test.go
+++ exchange_test.go
@@ -97,15 +97,17 @@ func TestImportWithInvalidData(t *testing.T) {
 	product.Meta(&exchange.Meta{Name: "Name"})
 	product.Meta(&exchange.Meta{Name: "Price"})
 
-	product.AddValidator(func(result interface{}, metaValues *resource.MetaValues, context *qor.Context) error {
-		if f, err := strconv.ParseFloat(fmt.Sprint(metaValues.Get("Price").Value), 64); err == nil {
-			if f == 0 {
-				return errors.New("product's price can't be env")
+	product.AddValidator(&resource.Validator{
+		Handler: func(result interface{}, metaValues *resource.MetaValues, context *qor.Context) error {
+			if f, err := strconv.ParseFloat(fmt.Sprint(metaValues.Get("Price").Value), 64); err == nil {
+				if f == 0 {
+					return errors.New("product's price can't be env")
+				}
+				return nil
+			} else {
+				return err
 			}
-			return nil
-		} else {
-			return err
-		}
+		},
 	})
 
 	if err := product.Import(csv_adaptor.New("fixtures/products.csv"), newContext()); err != nil {
@@ -123,10 +125,12 @@ func TestProcessImportedData(t *testing.T) {
 	product.Meta(&exchange.Meta{Name: "Name"})
 	product.Meta(&exchange.Meta{Name: "Price"})
 
-	product.AddProcessor(func(result interface{}, metaValues *resource.MetaValues, context *qor.Context) error {
-		product := result.(*Product)
-		product.Price = float64(int(product.Price * 1.1)) // Add 10% Tax
-		return nil
+	product.AddProcessor(&resource.Processor{
+		Handler: func(result interface{}, metaValues *resource.MetaValues, context *qor.Context) error {
+			product := result.(*Product)
+			product.Price = float64(int(product.Price * 1.1)) // Add 10% Tax
+			return nil
+		},
 	})
 
 	if err := product.Import(csv_adaptor.New("fixtures/products.csv"), newContext()); err != nil {
